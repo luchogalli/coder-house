a508ec4321605157ffacb8d827c2496887543a85
luchogallino <luchogalli@gmail.com>
2023-08-05 23:24:04 -0300

    probando clases

--- Java_gallino/JS/main.js
+++ Java_gallino/JS/main.js
@@ -1,18 +1,27 @@
-function Piloto(nombre, edad, horas_de_vuelo, vuelo){
-this.nombre = nombre;
-this.edad = edad;
-this.horas_de_vuelo = horas_de_vuelo;
-this.vuelo = vuelo;
+class Piloto {
+    constructor(nombre, edad, horas_de_vuelo, vuelo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.horas_de_vuelo = horas_de_vuelo;
+        this.vuelo = vuelo;
+    }
+
+    TextoInfoDePiloto(){
+        let texto = "";
+        texto += "Nombre: " + piloto_encontrado.nombre + "\n";
+        texto += "Edad: " + piloto_encontrado.edad + "\n";
+        texto += "Horas de vuelo: " + piloto_encontrado.horas_de_vuelo + "\n";
+    }
 }
 
 var pilotos = new Array();
-pilotos.push(new Piloto ("Carlos Medina", 58, 12000, 10));
-pilotos.push(new Piloto ("Marcos Perez", 38, 1800, 20));
-pilotos.push(new Piloto ("Carlos Menem", 55, 18000, 30));
-pilotos.push(new Piloto ("Diego Gutierrez", 46, 10500, 40));
-pilotos.push(new Piloto ("Jorge Macareno", 28, 8000, 50));
-pilotos.push(new Piloto ("Nikola Ksuftky", 20, 5000, 60));
-pilotos.push(new Piloto ("Zotroy Vladimuski", 41, 14400, 70));
+pilotos.push(new Piloto("Carlos Medina", 58, 12000, 10));
+pilotos.push(new Piloto("Marcos Perez", 38, 1800, 20));
+pilotos.push(new Piloto("Carlos Menem", 55, 18000, 30));
+pilotos.push(new Piloto("Diego Gutierrez", 46, 10500, 40));
+pilotos.push(new Piloto("Jorge Macareno", 28, 8000, 50));
+pilotos.push(new Piloto("Nikola Ksuftky", 20, 5000, 60));
+pilotos.push(new Piloto("Zotroy Vladimuski", 41, 14400, 70));
 
 let imput = prompt("Ingrese el numero de vuelo");
 
@@ -37,12 +46,9 @@ for (let index = 0; index < pilotos.length; index++) {
 
 if(encontre_piloto === true){
     let texto = "";
-    texto += "Nombre: " + piloto_encontrado.nombre + "\n";
-    texto += "Edad: " + piloto_encontrado.edad + "\n";
-    texto += "Horas de vuelo: " + piloto_encontrado.horas_de_vuelo + "\n";
+    texto += piloto_encontrado.TextoInfoDePiloto();
     alert(texto);
 }
-
 else{
     alert("No se encontro piloto");
 }
