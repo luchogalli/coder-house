e88d721fef862732890b88d23b8d60419c73fd84
luciano <luchogalli@gmail.com>
2023-09-09 17:09:54 -0300

    Update main.js

--- Java_gallino/JS/main.js
+++ Java_gallino/JS/main.js
@@ -29,52 +29,56 @@ class Piloto {
     }
 }
 
-async function ImportarPilotos(){
-    const json = await fetch("./JSONS/pilotos.json");
-    if (!json.ok) {
-        console.error("Error");
+async function ImportarPilotos() {
+    try {
+        const json = await fetch("./JSONS/pilotos.json");
+        if (!json.ok) {
+            console.error("Error fetching data");
+            return null;
+        }
+
+        const data = await json.json();
+
+        const pilotos = [];
+        for (let index = 0; index < data.pilotos.length; index++) {
+            const element = data.pilotos[index];
+            const infoVuelo = new InfoVuelo(element.info_vuelo.vuelo, element.info_vuelo.hora_salida, element.info_vuelo.hora_llegada);
+            pilotos.push(new Piloto(element.nombre, element.edad, element.horas_de_vuelo, infoVuelo));
+        }
+
+        return pilotos;
+    } catch (error) {
+        console.error("Error processing data:", error);
         return null;
     }
-    
-    const data = await json.json();
-
-    const pilotos = new Array();
-    for (let index = 0; index < data.pilotos.length; index++) {
-        const element = data.pilotos[index];
-        const infoVuelo = new InfoVuelo(element.info_vuelo.vuelo, element.info_vuelo.hora_salida, element.info_vuelo.hora_llegada);
-        pilotos.push(new Piloto(element.nombre, element.edad, element.horas_de_vuelo, infoVuelo));
-    }
-
-    return pilotos;
 }
 
-function Saludar(){
+function Saludar() {
     const iniciar_sesion = "iniciar_sesion";
     const ya_estuvo = localStorage.getItem(iniciar_sesion);
-    
+
     if (ya_estuvo) {
         alert("Bienvenido otra vez!");
-    }
-    else{
+    } else {
         localStorage.setItem(iniciar_sesion, true);
     }
 }
 
-function BuscarPiloto(numero){
+function BuscarPiloto(numero) {
     if (isNaN(numero)) {
-        alert("Valor invalido, vuelva a ingresar");
+        alert("Valor inválido, vuelva a ingresar");
         return;
     }
-    
+
     const piloto_anterior = "piloto_anterior";
     const piloto_anterior_vuelo = localStorage.getItem(piloto_anterior);
 
     let continuar = true;
     if (numero === piloto_anterior_vuelo) {
-        continuar = confirm("Este es el mismo piloto que buscaste la anterior vez, desear continuar?");
+        continuar = confirm("Este es el mismo piloto que buscaste la anterior vez, ¿desea continuar?");
     }
-    
-    if (continuar === false) {
+
+    if (!continuar) {
         return;
     }
 
@@ -82,31 +86,34 @@ function BuscarPiloto(numero){
 
     const numero_de_vuelo = parseInt(numero);
     const piloto_encontrado = pilotos.find(piloto => piloto.info_vuelo.vuelo === numero_de_vuelo);
-    const encontre_piloto = piloto_encontrado != null;
-    
-    if (encontre_piloto === true) {
+    const encontre_piloto = piloto_encontrado !== undefined;
+
+    if (encontre_piloto) {
         const texto = piloto_encontrado.TextoInfoDePiloto();
         alert(texto);
-    }
-    else {
-        alert("No se encontro piloto");
+    } else {
+        alert("No se encontró piloto");
     }
 }
 
-async function Core(){
+async function Core() {
     pilotos = await ImportarPilotos();
 }
 
 const botonCache = document.getElementById("boton-cache");
-botonCache.addEventListener("click", () => {
-    localStorage.clear();
-});
+if (botonCache) {
+    botonCache.addEventListener("click", () => {
+        localStorage.clear();
+    });
+}
 
 const botonInput = document.getElementById("boton-piloto");
-botonInput.addEventListener("click", () => {
-    const inputPiloto = document.getElementById("input-piloto");
-    BuscarPiloto(inputPiloto.value);
-});
+if (botonInput) {
+    botonInput.addEventListener("click", () => {
+        const inputPiloto = document.getElementById("input-piloto");
+        BuscarPiloto(inputPiloto.value);
+    });
+}
 
 let pilotos;
 
